bbd7748425b92f654b2bc1f9f0f7644b818974fc
Divya Mishra <69741860+Divya-whiteworld16@users.noreply.github.com>
2021-08-23 13:53:17 +0700

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,33 @@
+<?php include('topbar.php');?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/aboutus.css">
+</head>
+<body>
+
+<div class="about-section">
+  <h1>Developer</h1></h1>
+  <p>Hi,I am frontened developer. And i'm LGMVIP Intern.</p>
+
+</div>
+
+<h2 style="text-align:center"></h2>
+<div class="row">
+     <div class="column">
+          <div class="card">
+               <img src="img/developed.jpeg" alt="Divya Mishra" style="width:100%">
+               <div class="container">
+                    <h2>Divya Mishra</h2>
+                    <p class="title">Intern[LGMVIP]</p>
+                   <p>noreply@gmail.com</p>
+                    <p><a href="contactus.php"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+
+     
+</body>
+</html>
---
+++ contactus.php
@@ -0,0 +1,24 @@
+<?php include('topbar.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title></title>
+</head>
+<body style="background-color: skyblue;">
+    <br><br><br>
+    <div class="box" >
+    	<center><u><h1>Contact Us</h1></u></center>
+         <ul style="font-size: 30px;">
+         	<li>location - M.P</li>
+         	<li>City-Indore</li>
+         	<li>po - Kharling</li>
+         	<li>Dist - Indore-India</li>
+            <li>Pin - 765***</li>
+         	<li>contact no - +91 77********
+         	</li>
+         </ul>
+         <center><h2>Thank you</h2></center>
+    </div>
+</body>
+</html>
